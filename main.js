3a14be35b45bea5e54ca48340b5f9eaa412938cf
bodya17 <levantovychbogdan@gmail.com>
2016-07-10 12:05:59 +0300

    make everyone happy

--- main.js
+++ main.js
@@ -2,27 +2,16 @@ var app = angular.module('TimePicker', ['ngSanitize', 'ui.bootstrap', 'ui.select
 
 app.controller('TimePickerController', ['$scope', '$filter', function($scope, $filter) {
 
-    $scope.cities = moment.tz.names();
+    $scope.cities = moment.tz.names(); // fetch all locations
     $scope.city = {
       selected: $scope.cities[Math.floor(Math.random() * $scope.cities.length)]
     }
     
-    $scope.groupByCountry = (city) => city.substring(0, city.indexOf('/'));
+    $scope.country = (city) => city.substring(0, city.indexOf('/')); // get country
 
     $scope.getLocalTime = function() {
-        var offset = moment().tz($scope.city.selected).utcOffset() / 60;
-        const localOffset = moment().utcOffset() / 60,
-            sign = offset > 0 ? '+' : '-',
-            filteredtime = $filter('date')($scope.time, 'yyyy-MM-ddTHH:mm:ss');
-
-        offset = Math.abs(offset);
-        if (offset % 10 === offset) {
-            offset = "0" + offset;
-        }
-        
-        return moment.utc(filteredtime + sign + offset + ":00")
-            .add(localOffset, 'hour')
-            .format('HH:mm');
+        let m = moment.tz($filter('date')($scope.time, 'yyyy-MM-dd HH:mm'), $scope.city.selected);
+        return  m.tz(moment.tz.guess()).format("HH:mm");
     };
-  
+
 }]);
